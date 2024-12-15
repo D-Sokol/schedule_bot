2deadbb7c455b6b712717386087151a6df02dad9
Dmitrii Sokol <sokol.dmitry.f@gmail.com>
2024-12-15 20:16:08 +0000

    Add some dark magic to stop both bot and converter script.

--- converter.py
+++ converter.py
@@ -3,7 +3,11 @@ This script crops/resizes user images as a separate microservice to avoid lags i
 """
 import asyncio
 import json
+import logging
 import os
+import signal
+from asyncio import Event
+from contextlib import suppress
 
 import nats
 from nats.aio.msg import Msg
@@ -16,8 +20,27 @@ SAVE_NAME_HEADER = "Sch-Save-Name"
 RESIZE_MODE_HEADER = "Sch-Resize-Mode"
 TARGET_SIZE_HEADER = "Sch-Target-Size"
 
+logger = logging.getLogger(__name__)
 
-async def convert(js: JetStreamContext):
+
+def _set_event(event: Event, _handler: asyncio.events.Handle | None = None) -> None:
+    logger.info("Received signal, stopping converting...")
+    if _handler is not None:
+        _handler._run()  # noqa
+    event.set()
+
+
+async def _set_signals(event: Event):
+    loop = asyncio.get_running_loop()
+    with suppress(NotImplementedError, TypeError, AttributeError):
+        for sig in (signal.SIGTERM, signal.SIGINT):
+            # asyncio loop does not provide way to get current handler or add another one without removing it.
+            # Since aiogram set its own handlers to properly stop polling, we have to extract it and run both of them.
+            prev_handler: asyncio.events.Handle | None = loop._signal_handlers.get(sig)  # noqa
+            loop.add_signal_handler(sig, _set_event, event, prev_handler)
+
+
+async def convert(js: JetStreamContext, handle_signals: bool = True):
     async def callback(msg: Msg):
         save_name = msg.headers.get(SAVE_NAME_HEADER)
         resize_mode = msg.headers.get(RESIZE_MODE_HEADER)
@@ -25,17 +48,26 @@ async def convert(js: JetStreamContext):
         if resize_mode not in {"resize", "crop", "ignore"}:
             raise ValueError(f"Unknown resize mode: {resize_mode}")
 
+        logger.info("Converting %s with mode %s", save_name, resize_mode)
+
         # TODO: actually convert image.
         await store.put(save_name, msg.data)
         await msg.ack()
 
     store = await js.object_store(BUCKET_NAME)
     await js.subscribe(CONVERT_SUBJECT_NAME, cb=callback, durable="converter", manual_ack=True)
+    logger.info("Connected to NATS")
+
+    shutdown_event = Event()
+    if handle_signals:
+        logger.debug("Manage signal handlers")
+        await _set_signals(shutdown_event)
 
     try:
-        await asyncio.Future()
+        await shutdown_event.wait()
     except asyncio.CancelledError:
-        pass
+        logger.debug("Main task was cancelled")
+    logger.warning("Exiting main task")
 
 
 async def main(servers: str = "nats://localhost:4222"):
