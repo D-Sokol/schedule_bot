b03c949f1448c65a6c023e0e4de74f3cd0bea77e
Dmitrii Sokol <sokol.dmitry.f@gmail.com>
2025-03-26 11:23:59 +0000

    Fix importing missing classes.

--- bot_registry/__init__.py
+++ bot_registry/__init__.py
@@ -1,4 +1,4 @@
-from .image_assets import ElementsRegistryAbstract, MockElementRegistry, DbElementRegistry
-from .templates import TemplateRegistryAbstract, MockTemplateRegistry, DbTemplateRegistry
-from .texts import Schedule, ScheduleRegistryAbstract, MockScheduleRegistry, DbScheduleRegistry
-from .users import UserRegistryAbstract, MockUserRegistry, DbUserRegistry
+from .image_assets import ElementsRegistryAbstract, DbElementRegistry
+from .templates import TemplateRegistryAbstract, DbTemplateRegistry
+from .texts import Schedule, ScheduleRegistryAbstract, DbScheduleRegistry
+from .users import UserRegistryAbstract, DbUserRegistry
