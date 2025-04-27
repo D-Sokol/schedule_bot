a5eecbfed2ef024550073c4e36399dc444f491d6
Dmitrii Sokol <sokol.dmitry.f@gmail.com>
2025-04-27 14:25:08 +0000

    Get rendered images from temporary object store.

--- services/sender/__init__.py
+++ services/sender/__init__.py
@@ -8,9 +8,12 @@ from aiogram.exceptions import TelegramRetryAfter
 from aiogram.types import BufferedInputFile
 from nats.aio.msg import Msg
 from nats.js import JetStreamContext
+from nats.js.object_store import ObjectStore
 
+RESULT_BUCKET_NAME = "rendered"
 
-INPUT_SUBJECT_NAME = "schedules.ready"
+INPUT_RAW_SUBJECT_NAME = "schedules.ready"
+INPUT_STORE_SUBJECT_NAME = "schedules.ready_store"
 INPUT_SUBJECT_ERROR = "schedules.error"
 IMAGE_FORMAT = "png"
 
@@ -20,7 +23,7 @@ CHAT_ID_HEADER = "Sch-Chat-Id"
 logger = logging.getLogger(__name__)
 
 
-async def send(msg: Msg, bot: Bot, filename="Schedule.png") -> None:
+async def send_raw(msg: Msg, bot: Bot, filename="Schedule.png") -> None:
     if msg.headers is None:
         logger.error("Got message without headers")
         raise ValueError("Headers are required for message processing")
@@ -35,6 +38,24 @@ async def send(msg: Msg, bot: Bot, filename="Schedule.png") -> None:
     except TelegramRetryAfter as e:
         await msg.nak(e.retry_after)
 
+
+async def send_from_store(msg: Msg, bot: Bot, store: ObjectStore, filename="Schedule.png") -> None:
+    if msg.headers is None:
+        logger.error("Got message without headers")
+        raise ValueError("Headers are required for message processing")
+
+    chat_id = int(msg.headers[CHAT_ID_HEADER])
+    rendered_name = msg.data.decode()
+    result = await store.get(rendered_name)
+
+    try:
+        await bot.send_document(
+            chat_id=chat_id,
+            document=BufferedInputFile(file=result.data, filename=filename),
+        )
+        await msg.ack()
+    except TelegramRetryAfter as e:
+        await msg.nak(e.retry_after)
 
 async def response_error(msg: Msg, bot: Bot) -> None:
     if msg.headers is None:
@@ -53,7 +74,14 @@ async def response_error(msg: Msg, bot: Bot) -> None:
 
 
 async def sender_loop(js: JetStreamContext, bot: Bot, shutdown_event: asyncio.Event | None = None):
-    await js.subscribe(INPUT_SUBJECT_NAME, cb=partial(send, bot=bot), durable="sender", manual_ack=True)
+    store = await js.object_store(RESULT_BUCKET_NAME)
+    await js.subscribe(INPUT_RAW_SUBJECT_NAME, cb=partial(send_raw, bot=bot), durable="sender", manual_ack=True)
+    await js.subscribe(
+        INPUT_STORE_SUBJECT_NAME,
+        cb=partial(send_from_store, bot=bot, store=store),
+        durable="sender_store",
+        manual_ack=True,
+    )
     await js.subscribe(INPUT_SUBJECT_ERROR, cb=partial(response_error, bot=bot), durable="sender_err", manual_ack=True)
     logger.info("Connected to NATS")
 
