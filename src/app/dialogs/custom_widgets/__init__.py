b52eb84330fba381ea818a6a73196792a45775ee
Dmitrii Sokol <sokol.dmitry.f@gmail.com>
2025-07-20 10:41:58 +0000

    Add native button for user sharing in user selection window.

--- src/app/dialogs/custom_widgets/__init__.py
+++ src/app/dialogs/custom_widgets/__init__.py
@@ -1,7 +1,9 @@
 from .fluent_format import FluentFormat
+from .request_users import RequestUsers
 from .start_data import StartWithData
 
 __all__ = [
     "FluentFormat",
+    "RequestUsers",
     "StartWithData",
 ]
